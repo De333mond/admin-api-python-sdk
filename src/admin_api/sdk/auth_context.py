a72dd2c705a10323681d3fee6f2f0a74d11eabb9
Vladislav <vladb539@gmail.com>
2026-06-12 19:53:20 +0300

    add middlewares

--- src/admin_api/sdk/auth_context.py
+++ src/admin_api/sdk/auth_context.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Any
 
 from admin_api.grpc.dto.auth import UserData
 
@@ -7,3 +8,4 @@ from admin_api.grpc.dto.auth import UserData
 class AuthContext:
     user: UserData
     permissions: list[str]
+    middleware_result: dict[str, Any] = field(default_factory=dict)
--- src/admin_api/sdk/auth_manager.py
+++ src/admin_api/sdk/auth_manager.py
@@ -1,7 +1,12 @@
+from collections.abc import Callable
+from typing import TypeAlias
+
 from admin_api.exceptions import PermissionDenied
 from admin_api.grpc.auth_service import AuthGRPCService
 from admin_api.permissions.verifier import PermissionVerifier
 from admin_api.sdk.auth_context import AuthContext
+
+Middleware: TypeAlias = Callable[[AuthContext], dict | None]
 
 
 class AdminApiAuth:
@@ -10,17 +15,32 @@ class AdminApiAuth:
         self._timeout_ms = timeout_ms
         self._auth_service = AuthGRPCService(grpc_target=self._grpc_target)
         self._permission_verifiers: list[PermissionVerifier] = []
+        self._middlewares: list[Middleware] = []
 
     def add_permission_verifier(self, verifier: PermissionVerifier) -> None:
         self._permission_verifiers.append(verifier)
 
+    def set_middlewares(self, middlewares: list[Middleware]) -> None:
+        self._middlewares = middlewares
+
+    def _get_auth_context(self, service: AuthGRPCService, token: str) -> AuthContext:
+        payload = service.get_payload(jwt=token)
+        user = service.get_user_data(jwt=token)
+        return AuthContext(user=user, permissions=list(payload.permissions))
+
+    def _run_middlewares(self, auth_context: AuthContext) -> None:
+        for middleware in self._middlewares:
+            result = middleware(auth_context)
+            if result:
+                middleware_name = middleware.__name__
+                auth_context.middleware_result.update({middleware_name: result})
+
     def check(self, required: tuple[str, ...], token: str) -> AuthContext:
         with self._auth_service as service:
-            payload = service.get_payload(jwt=token)
-            user = service.get_user_data(jwt=token)
-            auth_context = AuthContext(user=user, permissions=list(payload.permissions))
+            auth_context = self._get_auth_context(service, token)
+            self._run_middlewares(auth_context)
 
-            if required and not any(permission in payload.permissions for permission in required):
+            if required and not any(permission in auth_context.permissions for permission in required):
                 raise PermissionDenied()
 
             for verifier in self._permission_verifiers:
